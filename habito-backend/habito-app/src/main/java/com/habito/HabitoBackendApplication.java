615dce99a6536ec0a55a0bea85fa1c347b7ae108
csaba.sallai <csaba.sallai@innoveo.com>
2024-05-17 15:54:28 +0200

    NOTICKET removing motivaa.properties, adding all the keycloak related entries to application-local.yaml

--- habito-backend/habito-app/src/main/java/com/habito/HabitoBackendApplication.java
+++ habito-backend/habito-app/src/main/java/com/habito/HabitoBackendApplication.java
@@ -2,10 +2,9 @@ package com.habito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+
 
 @SpringBootApplication
-@PropertySource("classpath:motivaa.properties")
 public class HabitoBackendApplication{
 
     public static void main(String[] args) {
