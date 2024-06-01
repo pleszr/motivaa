58d18657b8d9d1073e077359eec629706f9f059e
csabika98 <sallcsa.csaba8@gmail.com>
2024-06-01 02:26:49 +0200

    NOTICKET Frontend docker container created + main docker-compose created in the root directory

--- motivaa-frontend/src/keycloak.js
+++ motivaa-frontend/src/keycloak.js
@@ -4,8 +4,10 @@ import Keycloak from 'keycloak-js';
 
 const keycloakConfig = {
   url: 'http://localhost:8080', // base url keycloak
-  realm: 'motivaa', // realm name
-  clientId: 'login', // ClientID, equals with the Client that you have created
+  realm: 'Motivaa', // realm name
+  clientId: 'login',
+  clientSecret: 'txKdPjPpwbiUuuvEYyO9Is6QZoUGbOMN',
+  // ClientID, equals with the Client that you have created
 };
 
 const keycloak = new Keycloak(keycloakConfig);
