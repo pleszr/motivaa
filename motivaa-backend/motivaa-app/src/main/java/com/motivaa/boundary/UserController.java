29e35ac020517d4bcd534489a7e4f699458ea19f
pleszr <145920141+pleszr@users.noreply.github.com>
2024-07-13 23:43:06 +0200

    feat/#31 added documentation to users api

--- motivaa-backend/motivaa-app/src/main/java/com/motivaa/boundary/UserController.java
+++ motivaa-backend/motivaa-app/src/main/java/com/motivaa/boundary/UserController.java
@@ -1,22 +1,25 @@
 package com.motivaa.boundary;
 
-import com.motivaa.boundary.dto.CreateUserRequest;
-import com.motivaa.boundary.dto.CreateUserResponse;
-import com.motivaa.boundary.dto.FindUserByUuidRequestValidator;
-import com.motivaa.boundary.dto.FindUserByUuidResponse;
+import com.motivaa.boundary.dto.*;
 import com.motivaa.control.UserCreationService;
 import com.motivaa.control.UserFinder;
 import com.motivaa.entity.User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/user-apis")
+@Tag(name = "User API", description = "Endpoint to expose user management operations")
 @Log4j2
 public class UserController {
     UserCreationService userCreationService;
@@ -28,6 +31,28 @@ public class UserController {
         this.userFinder = userFinder;
     }
 
+    @Operation(summary = "Create a user")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "User created successfully",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = CreateUserResponse.class))
+                    }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid user data",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User not found",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content)
+    })
     @PostMapping("/users")
     public ResponseEntity<CreateUserResponse> createUser(@Valid @RequestBody CreateUserRequest createUserRequest) {
         log.trace("User creation for user started with details: {}", createUserRequest);
@@ -39,17 +64,57 @@ public class UserController {
         return ResponseEntity.ok(CreateUserResponse.fromUser(user));
     }
 
+    @Operation(summary = "Search all users")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returned users successfully",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = SearchAllUsersResponse.class))
+                    }),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Returned 0 users successfully",
+                    content = @Content(mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content)
+    })
     @GetMapping("/users")
-    public ResponseEntity<List<CreateUserResponse>> searchAllUsers() {
+    public ResponseEntity<List<SearchAllUsersResponse>> searchAllUsers() {
         List<User> users = userFinder.searchAllUsers();
-        List<CreateUserResponse> dtoList = users.stream().map(CreateUserResponse::fromUser).toList();
+        List<SearchAllUsersResponse> dtoList = users.stream().map(SearchAllUsersResponse::fromUser).toList();
+        if (dtoList.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(dtoList);
     }
 
-    //success_two_users
-    //success_0_users
-    //motivaa exception
-
+    @Operation(summary = "Find user by UUID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Returned user successfully",
+                    content = {
+                            @Content(mediaType = "application/json",
+                                    schema = @Schema(implementation = FindUserByUuidResponse.class))
+                    }),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid user UUID",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "User not found",
+                    content = @Content),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error",
+                    content = @Content)
+    })
     @GetMapping("/users/{userUuid}")
     public ResponseEntity<FindUserByUuidResponse> findUserByUuid(@PathVariable("userUuid") String userUuid) {
         FindUserByUuidRequestValidator.validateRequest(userUuid);
---
+++ motivaa-backend/motivaa-app/src/main/java/com/motivaa/boundary/dto/SearchAllUsersResponse.java
@@ -0,0 +1,32 @@
+package com.motivaa.boundary.dto;
+
+import com.motivaa.entity.User;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+public class SearchAllUsersResponse {
+    UUID uuid;
+    String email;
+    String firstName;
+    String lastName;
+    String status;
+    Timestamp createdTimestamp;
+    Timestamp lastLoginTimestamp;
+
+    private SearchAllUsersResponse(User user) {
+        this.uuid = user.getUuid();
+        this.email = user.getEmail();
+        this.firstName = user.getFirstName();
+        this.lastName = user.getLastName();
+        this.status = user.getStatus();
+        this.createdTimestamp = user.getCreatedTimestamp();
+        this.lastLoginTimestamp = user.getLastLoginTimestamp();
+    }
+
+    public static SearchAllUsersResponse fromUser (User user) {
+        return new SearchAllUsersResponse(user);
+    }
+}
