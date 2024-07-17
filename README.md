efd18f915128993f228979431f75363a991fc895
csabika98 <sallcsa.csaba8@gmail.com>
2024-07-17 00:55:04 +0000

    NOTICKET Creating API mocker based on the motivaa Swagger + small adjustments Docker/Frontend

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # motivaa
 
+
+# API mocker available
+It is bundled with frontend, if you start it will start the mocker too.
+
+```java
+Mock API server is running on http://localhost:3001
+In order to use API mocker, please run motivaa-backend, Swagger UI available at http://localhost:8093/v3/api-docs 
+```
 # HowTo Run Motivaa DOCKER - MacOS/Linux
 
 If you haven't installed it, please install 'make'
@@ -7,16 +15,17 @@ If you haven't installed it, please install 'make'
 1. To start with the dependencies, <b>to run ElasticSearch and Keycloak</b>, execute the following: (make sure Docker Compose is installed)
 
 ```c#
-make docker-up-ES-keycloak
+make docker-up-es-keycloak
 ```
-2. To run <b>motivaa-frontend</b>
-```c#
-make docker-up-frontend
-```
-3. To run <b>motivaa-backend</b>
+2. To run <b>motivaa-backend</b>
 ```c#
 make docker-up-backend
 ```
+3. To run <b>motivaa-frontend</b>
+```c#
+make docker-up-frontend
+```
+
 4. To <b>stop it</b>
 ```c#
 make docker-down
@@ -25,23 +34,23 @@ make docker-down
 
 Download and install Docker-Desktop -> https://docs.docker.com/desktop/install/windows-install/
 
-To start with the dependencies, <b>to run MySQL and Keycloak</b>, execute the following: (make sure Docker Desktop & Compose is installed)
+To start with the dependencies, <b>to run ElasticSearch and Keycloak</b>, execute the following: (make sure Docker Desktop & Compose is installed)
 
 1. Open <b>powershell</b> and execute the following.
 
 ```c#
-powershell -ExecutionPolicy ByPass -File .\runDocker.ps1 -Action --docker-up-mysql-keycloak
+powershell -ExecutionPolicy ByPass -File .\runDocker.ps1 -Action --docker-es-mysql-keycloak
 ```
 
-2. To run <b>motivaa-frontend</b>
+2. To run <b>motivaa-backend</b>
+```c#
+powershell -ExecutionPolicy ByPass -File .\runDocker.ps1 -Action --docker-up-backend
+```
+
+3. To run <b>motivaa-frontend</b>
 
 ```c#
 powershell -ExecutionPolicy ByPass -File .\runDocker.ps1 -Action --docker-up-frontend
-```
-
-3. To run <b>motivaa-backend</b>
-```c#
-powershell -ExecutionPolicy ByPass -File .\runDocker.ps1 -Action --docker-up-backend
 ```
 
 4. To <b>stop it</b>
---
+++ motivaa-frontend/mocker.mjs
@@ -0,0 +1,175 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import axios from 'axios';
+import swaggerUi from 'swagger-ui-express';
+import jsonServer from 'json-server';
+import bodyParser from 'body-parser';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const PORT = 3001;
+const SWAGGER_URL = 'http://localhost:8093/v3/api-docs';
+const DB_FILE = path.resolve(__dirname, 'db.json');
+
+function isValidRequestData(data, schema) {
+  return !!data;
+}
+
+async function createRoutesFromSwagger(swaggerUrl) {
+  try {
+    const response = await axios.get(swaggerUrl);
+    const swaggerDocument = response.data;
+
+    const server = jsonServer.create();
+    const router = jsonServer.router(DB_FILE);
+    const middlewares = jsonServer.defaults();
+
+    server.use(middlewares);
+    server.use(bodyParser.json());
+
+    for (const [path, pathDefinition] of Object.entries(swaggerDocument.paths)) {
+      for (const [method, methodDefinition] of Object.entries(pathDefinition)) {
+        const { responses, requestBody } = methodDefinition;
+
+        server[method.toLowerCase()](
+          path,
+          async (req, res) => {
+            try {
+              if (method.toLowerCase() === 'post' && path.endsWith('/users') && responses['200']) {
+                const requestSchema = requestBody?.content['application/json']?.schema;
+                if (!requestSchema) {
+                  console.error(`Schema not defined for ${method} ${path}`);
+                  return res.status(500).json({
+                    message: 'Internal Server Error'
+                  });
+                }
+
+                if (!isValidRequestData(req.body, requestSchema)) {
+                  return res.status(400).json({
+                    message: 'Invalid request body'
+                  });
+                }
+
+                const { firstName, lastName } = req.body;
+                if (!firstName || !lastName) {
+                  return res.status(400).json({
+                    message: 'firstName and lastName are required fields'
+                  });
+                }
+
+                const newUser = {
+                  uuid: uuidv4(),
+                  ...req.body,
+                  status: 'active',
+                  createdTimestamp: new Date().toISOString(),
+                  lastLoginTimestamp: new Date().toISOString()
+                };
+
+                router.db.get('users').push(newUser).write();
+
+                console.log(router.db.get('users').value());
+
+                res.status(200).json({
+                  message: 'User created successfully'
+                });
+              }
+              if (method.toLowerCase() === 'post' && path.endsWith('/createHabit') && responses['200']) {
+                const requestSchema = requestBody?.content['application/json']?.schema;
+                if (!requestSchema) {
+                  console.error(`Schema not defined for ${method} ${path}`);
+                  return res.status(500).json({
+                    message: 'Internal Server Error'
+                  });
+                }
+
+                if (!isValidRequestData(req.body, requestSchema)) {
+                  return res.status(400).json({
+                    message: 'Invalid request body'
+                  });
+                }
+
+                const { userUuid, name, recurringType } = req.body;
+                if (!userUuid || !name || !recurringType) {
+                  return res.status(400).json({
+                    message: 'userUuid, name, and recurringType are required fields'
+                  });
+                }
+
+                const newHabit = {
+                  uuid: uuidv4(),
+                  ...req.body,
+                  createdTimestamp: new Date().toISOString()
+                };
+
+                router.db.get('habits').push(newHabit).write();
+
+                console.log(router.db.get('habits').value());
+
+                res.status(200).json({
+                  message: 'Habit created successfully'
+                });
+              }
+
+              if (method.toLowerCase() === 'get' && path.endsWith('/createHabit') && responses['200'] && responses['200'].content['application/json'].schema) {
+                const responseData = router.db.get('habits').value();
+                res.status(200).json(responseData);
+              }
+
+              if (method.toLowerCase() === 'get' && responses['200'] && responses['200'].content['application/json'].schema) {
+                const { uuid } = req.query;
+                if (uuid) {
+                  const user = router.db.get('users').find({ uuid }).value();
+                  if (user) {
+                    res.status(200).json({
+                      users: [user]
+                    });
+                  } else {
+                    res.status(404).json({
+                      message: 'User not found'
+                    });
+                  }
+                } else {
+                  const responseData = router.db.get('users').value();
+                  res.status(200).json(responseData);
+                }
+              }
+            } catch (error) {
+              console.error('Error handling request:', error);
+              res.status(500).send('Internal Server Error');
+            }
+          }
+        );
+      }
+    }
+
+    server.use('/api', router);
+
+    server.use('/api-docs', async (req, res, next) => {
+      try {
+        swaggerUi.setup(swaggerDocument)(req, res, next);
+      } catch (error) {
+        res.status(500).send('Error rendering Swagger UI');
+      }
+    });
+
+    server.listen(PORT, () => {
+      console.log(`Mock API server is running on http://localhost:${PORT}`);
+      console.log(`In order to use this mocker, please run motivaa-backend, Swagger UI available at http://localhost:8093/v3/api-docs`);
+    });
+
+  } catch (error) {
+    console.error('Error fetching Swagger documentation:', error);
+  }
+}
+
+createRoutesFromSwagger(SWAGGER_URL);
+
+
+
+
+
+
+
